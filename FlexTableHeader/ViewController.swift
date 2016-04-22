afa4cb25f7a5d8bd0102460ab5d7677ddda55b47
Wenbin Zhang <wenbinzhang0802@gmail.com>
2016-04-22 09:27:08 -0700

    Initial Commit

---
+++ FlexTableHeader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlexTableHeader
+//
+//  Created by Wenbin Zhang on 4/22/16.
+//  Copyright © 2016 Wenbin Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
